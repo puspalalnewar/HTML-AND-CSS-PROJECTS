a2c469b79998ab41696d87b071cd590f42dcb87b
Puspalal <puspalalnewar79@gmail.com>
2025-01-10 19:54:01 +0530

    Updated

--- Scroll Top/app.js
+++ Scroll Top/app.js
@@ -1,7 +1,26 @@
-const stickyBtn = document.getElementById('stickyBtn');
-const navbar = document.querySelector(".navbar");
-function scrollTop() {
-    navbar.scrollIntoView({ behavior: "smooth" });
-}
+// const stickyBtn = document.getElementById('stickyBtn');
+// const navbar = document.querySelector(".navbar");
+// function scrollTop() {
+//     navbar.scrollIntoView({ behavior: "smooth" });
+// }
 
-stickyBtn.addEventListener("click", scrollTop);
+// stickyBtn.addEventListener("click", scrollTop);
+
+// 
+
+const stickyBtn = document.getElementById('stickyBtn');
+
+window.addEventListener("scroll", ()=>{
+    if(window.pageYOffset > 20){
+        stickyBtn.style.display = "block";
+    }else{
+        stickyBtn.style.display = "none";
+    }
+})
+
+stickyBtn.addEventListener("click", ()=>{
+    window.scrollTo({
+        top : 0,
+        behavior : "smooth",
+    })
+})
