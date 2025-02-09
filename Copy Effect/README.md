6aca2bb4450f3ca5deb7e75012a31c8575bfff32
Puspalal <puspalalnewar79@gmail.com>
2025-02-09 20:02:40 +0530

    Add readme.md

---
+++ Copy Effect/README.md
@@ -0,0 +1,13 @@
+# Copy Effect
+
+> Screenshot reference
+
+    1) First Screenshot
+
+![image](./Images/first.png)
+
+    2) Screenshot after paste the text
+
+![image](./Images/second.png)
+
+Credits:- [Puspalal Newar](https://github.com/puspalalnewar)
--- Copy Effect/script.js
+++ Copy Effect/script.js
@@ -1,5 +1,5 @@
 const content = document.querySelector(".text");
 document.addEventListener("copy", (e)=>{
     e.preventDefault();
-    e.clipboardData.setData("text/plain", "Your are Hacked!!");
+    e.clipboardData.setData("text/plain", "You are Hacked!!");
 })
