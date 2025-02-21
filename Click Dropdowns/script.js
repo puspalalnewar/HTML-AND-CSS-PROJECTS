98ee2a73b88a4f2e8fb32058b2d2e92bccf8d6ac
Puspalal <puspalalnewar79@gmail.com>
2025-02-21 13:25:29 +0530

    Text Shadows

--- Click Dropdowns/script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector(".dropdown-btn");
-const content = document.querySelector(".dropdown-content");
-
-btn.addEventListener("click", () => {
-    content.classList.toggle("dropdown-active");
-})
