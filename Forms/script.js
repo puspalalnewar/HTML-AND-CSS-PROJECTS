7f3c4fc60a81f64b3ac346120924990735c19159
Puspalal <puspalalnewar79@gmail.com>
2025-02-28 12:15:50 +0530

    Radio Buttons

--- Forms/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("myForm").addEventListener("submit", function (event) {
-    event.preventDefault();
-    document.getElementById("message").textContent = "Form submitted successfully!";
-});
