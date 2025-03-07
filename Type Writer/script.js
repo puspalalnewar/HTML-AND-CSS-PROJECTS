6e0d9135025cb9278c0028d71b13c41770d81c50
Puspalal <puspalalnewar79@gmail.com>
2025-03-07 21:33:21 +0530

    Type Writer

--- Type Writer/script.js
+++ Type Writer/script.js
@@ -1,7 +1,12 @@
 const para = document.querySelector(".text");
 
-
-let position = ["Student", "Instructor","Frontend Developer", "Learner", "MERN Stack Developer"];
+let position = [
+  "Student",
+  "Instructor",
+  "Frontend Developer",
+  "Learner",
+  "MERN Stack Developer",
+];
 
 let positionIdx = 0;
 
@@ -9,16 +14,17 @@ let charIdx = 0;
 
 updatePos();
 
-function updatePos(){
-    charIdx++;
-    para.innerHTML = `${position[positionIdx].slice(0,1) === "I" ? "an " : "a "} ${position[positionIdx].slice(0, charIdx)}`;
-    if(charIdx === position[positionIdx].length){
-        positionIdx++;
-        charIdx = 0;
-    }
-    if(positionIdx === position.length){
-        positionIdx = 0;
-    }
-    setTimeout(updatePos, 400);
-
+function updatePos() {
+  charIdx++;
+  para.innerHTML = `${
+    position[positionIdx].slice(0, 1) === "I" ? "an " : "a "
+  } ${position[positionIdx].slice(0, charIdx)}`;
+  if (charIdx === position[positionIdx].length) {
+    positionIdx++;
+    charIdx = 0;
+  }
+  if (positionIdx === position.length) {
+    positionIdx = 0;
+  }
+  setTimeout(updatePos, 400);
 }
